02f6ee3e87e4721ecaa4b28e65373d68b3dd6cc1
vinicius <viniciussouza886@gmail.com>
2024-03-12 19:49:36 -0300

    mudança de estatico para dinamico

--- controllers/routes.py
+++ controllers/routes.py
@@ -1,20 +1,35 @@
-from flask import render_template
+from flask import render_template, request
+
+jogadores = []
+
+jogos = []
+
+gamelist = [{'Título' : 'CS-GO', 'Ano' : 2012, 'Categoria' : 'FPS Online'}]
 
 def init_app(app):
-    
+    # Definindo a rota principal
     @app.route('/')
-
+    # Função que será executada ao acessar a rota
     def home():
+        # Retorno que será exibido na rota
         return render_template('index.html')
 
-    @app.route('/titulos')
-    def titulos():
-        titulo = 'Campeonato santista de 1913'
-        ano = 1913
-        return render_template('titulos.html',titulo=titulo, ano=ano)
+    @app.route('/games', methods=['GET', 'POST'])
+    def games():
+        game = gamelist[0]
+        
+        if request.method == 'POST':
+            if request.form.get('jogador'):
+                jogadores.append(request.form.get('jogador'))
+    
+            if request.form.get('jogos'):
+                jogos.append(request.form.get('jogos'))
+        return render_template('games.html', game=game, jogos=jogos, jogadores=jogadores)
 
-    @app.route('/jogadores')
-    def jogador():
-        jogador = ['Neymar','Ganso']
-        contratacao = ['2009','2005']
-        return render_template('jogadores.html',jogador=jogador,contratacao=contratacao)
+    @app.route('/cadgames', methods=['GET', 'POST'])
+    def cadgames():
+        if request.method == 'POST':
+            if request.form.get('titulo') and request.form.get('ano') and request.form.get('categoria'):
+                gamelist.append({'Título': request.form.get('titulo'), 'Ano' : request.form.get('ano'), 'Categoria' : request.form.get('categoria')})
+        
+        return render_template('cadgames.html', gamelist=gamelist)
